d0001ccef6fb89353ded3e797621f75c3615b232
Renata Gerecke <renatavgerecke@gmail.com>
2024-12-04 20:26:20 -0500

    integrate socrata code

--- 01_getdata.py
+++ 01_getdata.py
@@ -1,45 +1,26 @@
 # import and save NYC Jobs data from Open Data
 
-import numpy as np
-import pandas as pd
-# from socrata.authorization import Authorization
-# from socrata import Socrata 
-import requests
+from socrata import socrata_api_query
 from datetime import datetime
 
-odata_url = "https://data.cityofnewyork.us/resource/pda4-rgn4.json"
-
-df = pd.read_json(odata_url+"?$limit=10000")
-
-def dl_data(url="https://data.cityofnewyork.us/resource/pda4-rgn4.json"):
-    """
-    Downloads data from NYC Open Data Socrata API and returns it as a pandas DataFrame
+if __name__ == '__main__':
+    print('NYC job postings active this week')
+    start_time = time.time()
+    od_df = socrata_api_query(
+        dataset_id='pda4-rgn4', 
+        timeout=300,
+        limit=10000
+        )
+    end_time = time.time()
+    len_sec = end_time - start_time
+    min_time = math.floor(len_sec / 60)
+    print(
+        f'Duration: {min_time} min'
+        f' {len_sec - min_time * 60:.4} sec'
+    )
     
-    Parameters:
-    url (str): The URL for the Socrata API endpoint
-    
-    Returns:
-    pandas.DataFrame: The downloaded data as a DataFrame
-    str: The timestamp when the data was downloaded
-    """
-    try:
-
-
-
-        # Download data
-        df = pd.read_json(url+"?$limit=10000")
+    csv_filename = f'nyc_jobs_{datetime.now().strftime("%Y%m%d_%H%M%S")}.csv'
+    od_df.to_csv(f'raw/{csv_filename}', index=False)
         
-        # Save DataFrame to CSV
-        csv_filename = f'nyc_jobs_{datetime.now().strftime("%Y%m%d_%H%M%S")}.csv'
-        df.to_csv(f'raw/'+csv_filename, index=False)
-        
-        print(f"CSV saved to {csv_filename}")
-        print(f"Number of records downloaded: {len(df)}")
-        
-        return df
-        
-    except requests.exceptions.RequestException as e:
-        print(f"Error downloading data: {e}")
-        return None, None
-    
-df = dl_data()
+    print(f"CSV saved to {csv_filename}")
+    print(f"Number of records downloaded: {len(od_df)}")
